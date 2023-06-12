dfe2eb4ddf49353d18f38a65262e7ffd12b43104
Rafael Gomes <maciel.rafael1@gmail.com>
2023-06-12 14:47:05 -0400

    create filter for manufacturer

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import CustomButtom from "./CustomButton";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { CustomButtom, Footer, Hero, Navbar };
+export {
+  CustomButtom,
+  CustomFilter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchBar,
+  SearchManufacturer,
+};
